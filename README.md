db91d728b6fcb56e07afe3deff792932929e4185
Chris <chris.s.hodder@gmail.com>
2021-05-22 15:28:01 +0100

    WIP: Basic interactiveness, setting up loop and input cleaning. Added ability to quit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rust-paper-scissors
-Rock Paper Scissors in rust
+Rock Paper Scissors in Rust.
+
+This is roughly based on the [Guessing Game](https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html) tutorial in the Rust Programming Language book.
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ use std::io;
 fn main() {
     println!("What's your name?");
 
+    let options = ["rock", "paper", "scissors", "r", "p", "s", "q", "quit"];
+    let mut computer_moves = ["rock", "paper", "scissors"];
     let input_handler = io::stdin();
     let mut name = String::new();
     let mut player_move = String::new();
+
     input_handler
         .read_line(&mut name)
         .expect("Failed to read line");
@@ -13,9 +16,35 @@ fn main() {
     name = name.trim().to_string();
 
     println!("Hello, {}!", name);
-    println!("Let's play Rust Paper Scissors! What do you choose? (r)ock, (p)aper, or (s)cissors?");
+    println!("Let's play Rust Paper Scissors! Pick (r)ock, (p)aper, or (s)cissors or (q)uit");
 
-    input_handler.read_line(&mut player_move).expect("Failed to read line");
+    loop {
+        player_move = String::new();
+        input_handler.read_line(&mut player_move).expect("Failed to read line");
+        player_move = normalise_input(player_move);
 
-    println!("You picked {}!", player_move.trim());
+        if !options.contains(&&*player_move) {
+            println!("Invalid input '{}', please pick from:", player_move);
+            for value in options.iter(){
+                println!("  {}", value);
+            }
+        }
+
+        if player_move == "q" || player_move == "quit" {
+            break;
+        }
+
+        println!("You picked {}!", player_move.trim());
+    }
+
+    println!("Thanks for playing!")
+
+}
+
+
+fn normalise_input(input: String) -> String{
+    let mut output = input.to_lowercase().to_string();
+    output = output.trim().to_string();
+
+    output
 }
